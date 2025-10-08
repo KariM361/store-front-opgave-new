a48e4c6dde822665a1a8deb8e068f570553f4698
Kari idskov holm Madsen <kama7@elev.techcollege.dk>
2025-10-08 10:25:17 +0200

    new

--- Js/Modules/view.js
+++ Js/Modules/view.js
@@ -6,15 +6,18 @@ export function renderProductcard(product) {
   let cardTitle = document.createElement('h2');
   let cardImage = document.createElement('img');
   let cardPrice = document.createElement('p');
+  let description = document.createElement('p')
 
   cardTitle.innerText = product.title;
   cardImage.src = product.thumbnail;
   cardImage.alt = product.title;
   cardPrice.innerText = product.price + " kr.";
+  description.innerText = product.description;
 
   card.appendChild(cardImage);
   card.appendChild(cardTitle);
   card.appendChild(cardPrice);
+  card.appendChild(description);
 
   // Tilføj klik-event for at vise detaljer
   card.addEventListener('click', () => renderProductDetails(product));
